5778662bd03bbc6ba7f4342329639495fd1f7030
Tony Lam <Tonylam750@outlook.com>
2025-06-14 01:26:22 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,53 @@
+export const tweetsData = [   
+    {
+        handle: `@Elon 💎`,
+        profilePic: `images/musk.png`,
+        likes: 42069,
+        retweets: 1001,
+        tweetText: `Imagine being rushed in 2025 #rushedInClashOfClans`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Zeno69 ✅`,
+        profilePic: `images/zeno.png`,
+        likes: 68,
+        retweets: 419,
+        tweetText: `Jeg er stolt av å være rushed i clash of clans🚀`,
+        replies: [
+                  {
+                handle: `@YoHanne ✅`,
+                profilePic: `images/johanne.png`,
+                tweetText: `Du bringer skam over familien din😑`,
+            },
+                  {
+                handle: `@Henkemann ✅`,
+                profilePic: `images/henrik.jpg`,
+                tweetText: `Tror ikke muttern er så stolt av deg nå nei🤡`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@ArbeidsløsGutt420`,
+        profilePic: `images/tony.png`,
+        likes: 999,
+        retweets: 99,
+        tweetText: `pls gi meg jobb😫 #jegKanVæreSnacksAnsvarlig`,
+        replies: [
+            {
+                handle: `@FremtidigArbeidsGiver420 ☣️`,
+                profilePic: `images/gud.jpg`,
+                tweetText: `Wow sjekket basen din og så at du ikke er rushed. Dermen vil jeg med glede tilby deg jobb hos oss!`,
+            },
+           
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
